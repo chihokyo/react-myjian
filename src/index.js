c28126f18298a805fd45afa47f695a7965192244
chihokyo <chihoukyo@gmail.com>
2020-09-17 16:44:55 +0900

    【step1】stylecomponent&resetcss导入

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// 第三方模块 styled-components 下的引入
+import './style.js'
 
 ReactDOM.render(
   <React.StrictMode>
