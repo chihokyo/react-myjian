fe6560b293c39ecd430d41ab0e45db6802f293be
chihokyo <chihoukyo@gmail.com>
2020-09-23 15:17:57 +0900

    【step1】首页数据ajax化

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,8 @@ import Topic from './components/Topic'
 import List from './components/List'
 import Recommend from './components/Recommend'
 import Writer from './components/Writer'
+import axios from 'axios'
+import { connect } from 'react-redux'
 
 import {
   HomeWrapper,
@@ -25,6 +27,30 @@ class Home extends Component {
       </HomeWrapper>
     )
   }
+
+  componentDidMount() {
+    axios.get('/api/home.json')
+      .then((res) => {
+        const result = res.data.data
+        const action = {
+          type: 'change_home_data',
+          topicList: result.topicList,
+          articleList: result.articleList,
+          recommendList: result.recommendList,
+        }
+        this.props.changeHomeData(action)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 }
 
-export default Home
+
+const mapDispatch = (dispatch) => ({
+  changeHomeData(action) {
+    dispatch(action)
+  }
+})
+
+export default connect(null, mapDispatch)(Home)
