1cda0a6fd97a77effd7685ee2fcf24ee66e432c1
chihokyo <chihoukyo@gmail.com>
2020-09-24 14:34:31 +0900

    【step1】detail页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
-
+import { DetailWrapper, Header, Content } from './style'
 class Detail extends Component {
   render() {
-    return <div>Detail</div>
+    return (
+      <DetailWrapper>
+        <Header>这里是详情页的标题：详情页的标题呢</Header>
+        <Content>
+          <img src='https://www.gamebyte.com/wp-content/uploads/2018/11/Naturo-to-Boruto-shinobi.jpeg' alt='img' />
+          <p><b>这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容</b></p>
+          <p>这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容</p>
+          <p>这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容</p>
+          <p>这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容这里是详情内容</p>
+        </Content>
+      </DetailWrapper>
+    )
   }
 }
 
