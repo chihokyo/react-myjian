2357c1987fc3556d94619ed5744e8040e0ccf8b3
chihokyo <chihoukyo@gmail.com>
2020-09-24 16:12:50 +0900

    【step1】登录页面静态

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './common/header/index'
 import Home from './pages/home'
 import Detail from './pages/detail'
+import Login from './pages/login'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 import store from './store'
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Route path='/' exact component={Home}></Route>
+        <Route path='/login' exact component={Login}></Route>
         <Route path='/detail/:id' exact component={Detail}></Route>
       </BrowserRouter>
     </Provider>
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import logoPic from '../../static/logo.png'
 
 export const HeaderWrapper = styled.div`
+  z-index: 1;
   position: sticky;
   top: 0;
   border-bottom: 1px solid #f0f0f0;
